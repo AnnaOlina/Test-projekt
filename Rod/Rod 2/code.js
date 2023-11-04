72c5afe4279dbb19fc6d0c25b4b3e312f2a3b852
AnnaOlina <aoelundin@gmail.com>
2023-11-04 11:37:43 +0100

    Opgave med KL while løkke med sætning

--- Rod/Rod 2/code.js
+++ Rod/Rod 2/code.js
@@ -7,3 +7,25 @@ while (counter < list.length) {
 	console.log(list[counter])
 	counter = (counter + 1)
 }
+
+console.log("hest")
+console.log(list[3])
+
+console.log("hop")
+console.log(list[2])
+
+console.log("hest" + "hop")
+console.log(list[3] + " " + list[2])
+
+let tekst = list[3] + " " + list[2]
+console.log(tekst)
+
+let sætning = "" // definer/opret variablen "sætning" og giv den værdien "" (tom tekst)
+
+let index = 0 // definer/opret variablen "index" og giv den værdien 0
+while (index < list.length) { // start en while løkke. Så længe "index < list.length" evaluerer til true, så kører den koden i { }
+	sætning = sætning + list[index] + " " // evaluer "værdi + list[index] + " "" og gem det i variablen "sætning"
+	index = index + 1 // evaluer "index + 1" og gem det i variablen "index"
+}
+
+console.log(sætning) // hent værdien fra variablen "sætning" og log det i konsollen
